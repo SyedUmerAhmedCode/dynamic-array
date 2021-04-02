1248e67fe1d2709811c99b2193bb1b7d62ba6124
sara-fatima321 <46785812+sara-fatima321@users.noreply.github.com>
2021-04-02 21:55:44 +0200

    dynamic-array: Change in code to double check git name property on github.

--- src/main/EntryPoint.java
+++ src/main/EntryPoint.java
@@ -5,9 +5,9 @@ public class EntryPoint {
 	public static void main(String[] args) {
 		DynamicArray<Integer> dynamicArray=new DynamicArray<Integer>(5);
 		dynamicArray.add(1);
-		dynamicArray.add(2);
 		dynamicArray.add(3);
-		dynamicArray.add(4);
+		dynamicArray.add(5);
+		dynamicArray.add(7);
 		
 		System.out.println(dynamicArray.toString());
 	}
